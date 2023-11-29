7c671235c3369b449e474cf681301656852beb24
Kert619 <108245652+Kert619@users.noreply.github.com>
2023-11-29 17:52:43 +0800

    add lesson03 exercise and lesson04

---
+++ lesson04-computed-properties/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref, computed } from "vue";
+const books = ref([
+  "Vue 2 - Advanced Guide",
+  "Vue 3 - Basic Guide",
+  "Vue 4 - The Mystery",
+]);
+
+const hasBooks = computed(function () {
+  return books.value.length > 0 ? "Yes" : "None";
+});
+
+const number = ref(10);
+const timesTwo = computed(function () {
+  return number.value * 2;
+});
+</script>
+
+<template>
+  <h1>Has books: {{ hasBooks }}</h1>
+  <h1>Number: {{ timesTwo }}</h1>
+</template>
+
+<style scoped></style>
