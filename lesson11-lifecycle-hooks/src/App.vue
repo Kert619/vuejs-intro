cf08653ce47db5ac8193aac9c92949024338ca5f
Kert619 <108245652+Kert619@users.noreply.github.com>
2023-12-02 21:26:30 +0800

    add lessons

---
+++ lesson11-lifecycle-hooks/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(function () {
+  const input = document.querySelector("input");
+  input.focus();
+  console.log(input);
+});
+</script>
+
+<template>
+  <div class="m-5 p-5">
+    <input type="text" class="form-control" placeholder="First Name" />
+  </div>
+</template>
+
+<style scoped></style>
---
+++ lesson12-template-ref/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, ref } from "vue";
+
+const inputRef = ref(null);
+
+onMounted(function () {
+  console.log(inputRef.value);
+  inputRef.value.focus();
+});
+</script>
+
+<template>
+  <div class="m-5 p-5">
+    <input
+      ref="inputRef"
+      type="text"
+      class="form-control"
+      placeholder="First Name"
+    />
+  </div>
+</template>
+
+<style scoped></style>
